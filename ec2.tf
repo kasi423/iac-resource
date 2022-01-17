9331f6256d07dfa99aa9c8785f64fd3526895d44
kasi423 <ch.kasirao023@gmail.com>
2022-01-17 13:16:54 +0530

    added ec2 tf file

--- ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "main" {
-  ami           = "ami-08e4e35cccc6189f4"
-  key_name      = "tower43"
-  instance_type = "t2.micro"
-  tags= {
-    Name = "jenkins_instance"
-  }
-}
