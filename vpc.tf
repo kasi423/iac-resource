a175e6849f8597f0a7513a516a636e8559bd8f7d
kasi423 <38430122+kasi423@users.noreply.github.com>
2022-01-19 17:08:46 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -14,3 +14,20 @@ resource "aws_default_subnet" "default_az1" {
     Name = "Default subnet for us-east-1a"
   }
 }
+resource "aws_default_route_table" "example" {
+  default_route_table_id = aws_vpc.example.default_route_table_id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+  }
+
+  tags = {
+    Name = "Pub_RT"
+  }
+}
