c1b95565372a7696f165a883b6eda280d305ce25
kasi423 <38430122+kasi423@users.noreply.github.com>
2022-01-19 18:14:15 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -25,7 +25,7 @@ resource "aws_default_route_table" "Public" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.main.id
+    gateway_id = aws_internet_gateway.gw.id
   }
 tags = {
     Name = "Pub_RT"
