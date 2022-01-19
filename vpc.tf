84ba412217c3a6413035db5dbaa2185708240ef4
kasi423 <38430122+kasi423@users.noreply.github.com>
2022-01-19 18:16:56 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -21,8 +21,9 @@ resource "aws_internet_gateway" "gw" {
     Name = "main"
   }
 }
-resource "aws_default_route_table" "Public" {
+resource "aws_default_route_table" "public" {
   vpc_id = aws_vpc.main.id
+  default_route_table_id = aws_vpc.public.default_route_table_id
   route {
     cidr_block = "10.0.1.0/24"
     gateway_id = aws_internet_gateway.gw.id
