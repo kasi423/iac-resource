2b778c5e1ef6d3324e61085f70f52476ad678b70
kasi423 <ch.kasirao023@gmail.com>
2022-01-19 20:27:42 +0530

    added new vpc

--- variable.tf
+++ variable.tf
@@ -1,17 +1,17 @@
 variable "vpc-cidr" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = 10.0.0.0/16
   description      = "vpc cidr_block"
   type             = string
 }
 
-variable "public-subnet1-cidr" {
-  cidr_block       = "10.0.1.0/24"
+variable "c" {
+  cidr_block       = 10.0.1.0/24
   description      = "public-subnet1-cidr"
   type             = string
 }
 
 variable "public-subnet2-cidr" {
-  cidr_block       = "10.0.2.0/24"
+  cidr_block       = 10.0.2.0/24
   description      = "public-subnet2-cidr"
   type             = string
 }
