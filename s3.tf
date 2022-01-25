4bde8fb129e9a5d0de809a6e50b8efc6803392ab
kasi423 <ch.kasirao023@gmail.com>
2022-01-25 16:43:00 +0530

    added s3

--- s3.tf
+++ s3.tf
@@ -1,7 +1,9 @@
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket-45891"
   acl    = "private"
-
+  versioning {
+    enabled = true
+  }
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
