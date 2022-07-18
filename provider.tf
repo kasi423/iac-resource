12b35fd7ecc6fa47a5dcc1d60755c32c879a80af
kasi423 <38430122+kasi423@users.noreply.github.com>
2022-07-18 19:23:46 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key  = "AKIATMFLBAZEX6X66RXQ"
-  secret_key  = "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"
+  access_key  = "AKIAVFLXMWKCMD7OC462"
+  secret_key  = "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"
   region = "us-east-1"
 }
